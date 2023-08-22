e04c08ff6e3d49e06df9739ced05f58f8450de25
kyrov <pho445.2013@gmail.com>
2023-08-23 01:32:45 +0700

    Fixed the error in Main.v

--- Lab02/Source/Main.v
+++ Lab02/Source/Main.v
@@ -27,15 +27,26 @@ module Main(
     input wire clock 
     );
     reg [3:0] Nin;
-    wire [1:0] clk;
-    ClockDivider #(.N(20),.M(2)) u0(clock,clk);
+    reg [20:0] count;
     Encoder u1(Nin,C_LED);
-    always @(clk) begin
-        case (clk)
+ //   always @(posedge clock) begin
+    always @(posedge clock) begin
+        case (count[20:19])
             2'b00 : begin A_LED <= 4'b1110; Nin <= In[3:0]; end
             2'b01 : begin A_LED <= 4'b1101; Nin <= In[7:4]; end
             2'b10 : begin A_LED <= 4'b1011; Nin <= In[11:8]; end
             2'b11 : begin A_LED <= 4'b0111; Nin <= In[15:12]; end
         endcase
+        count <= count+1;
     end
+    
+    
+//    reg [5:0] cnt;
+//    always @(posedge clk) begin
+//        cnt <= cnt + 1;
+//    end
+//    assign clk2 = cnt[2];
+//    always@(posedge clk2) begin
+      
+//    end
 endmodule
